3316dc92a031188a4a0106fdf74f4cc014d53e0b
Conor Houghton <cscjh@eduroam-27-190.nomadic.bris.ac.uk>
2017-08-15 16:25:48 +0100

    m

--- code/line.py
+++ code/line.py
@@ -7,10 +7,9 @@ tom=Turtle()
 tom.forward(100)
 
 #these two lines are for saving the image
-#ts = tom.getscreen()
-#ts.getcanvas().postscript(file="line.eps")
-
-#ts.setup(400,400)
+ts = tom.getscreen()
+ts.setup(400,400)
+ts.getcanvas().postscript(file="line.eps")
 
 tom.getscreen()._root.mainloop()
 
--- code/turtle_doing_nothing.py
+++ code/turtle_doing_nothing.py
@@ -3,10 +3,11 @@ from turtle import *
 tom=Turtle()
 
 #these two lines are for saving the image
-#ts = tom.getscreen()
-#ts.getcanvas().postscript(file="line.eps")
+ts = tom.getscreen()
+ts.setup(400,400)
+ts.screensize(canvwidth=400, canvheight=400, bg=None)
+ts.getcanvas().postscript(file="turtle_doing_nothing.eps")
 
-#ts.setup(400,400)
 
 tom.getscreen()._root.mainloop()
 

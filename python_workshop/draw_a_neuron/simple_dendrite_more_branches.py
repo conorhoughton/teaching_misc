cf99e95b5660ff2342d2b9eec7fd473ce8d43698
Conor <conor@localhost.(none)>
2017-08-28 23:36:17 +0100

    tweaks and the soma

--- python_workshop/draw_a_neuron/simple_dendrite_more_branches.py
+++ python_workshop/draw_a_neuron/simple_dendrite_more_branches.py
@@ -2,18 +2,27 @@ from turtle import *
 from math import *
 import random as rnd
 
-def get_angle():
-    return rnd.normalvariate(angle,sigma)
+def get_angle(sigma):
+#    return rnd.normalvariate(angle,sigma)
+    return 2*sigma*rnd.random()-sigma
 
-def dendrite(length,width):
+def bendy_line(length,jacob):
+    mini_length=length/5.
+    for i in range(5):
+        jacob.forward(mini_length)
+        jacob.left(rnd.normalvariate(0,5))
+
+
+def dendrite(length,width,branch_n,factor,sigma,jacob):
     
     if width==0:
         return
 
-    tom.width(width)
+    jacob.width(width)
 
-    x=tom.xcor()
-    y=tom.ycor()
+    x=jacob.xcor()
+    y=jacob.ycor()
+    heading=jacob.heading()
 
     branches=[]
     number_branches=2+rnd.randrange(branch_n-2)
@@ -23,49 +32,47 @@ def dendrite(length,width):
     
     while len(branches)!=0:
         next_branch=min(branches)
-        tom.forward(next_branch)
-        if rnd.random()<0.5:
-            tom.right(get_angle())
-            dendrite(length*factor,width-1)
-            tom.left(get_angle())
-        else:
-            tom.left(get_angle())
-            dendrite(length*factor,width-1)
-            tom.right(get_angle())
+        bendy_line(next_branch,jacob)
+        angle=get_angle(sigma)
+        jacob.right(angle)
+        dendrite(length*factor,width-1,branch_n,factor,sigma,jacob)
+        jacob.left(angle)
     
         branches=[b-next_branch for b in branches if b!=next_branch]
         
 
 
-    tom.penup()
-    tom.setx(x)
-    tom.sety(y)
-    tom.width(width+1)
-    tom.pendown()
+    jacob.penup()
+    jacob.setx(x)
+    jacob.sety(y)
+    jacob.setheading(heading)
+    jacob.width(width+1)
+    jacob.pendown()
     return
 
 
-branch_n=5
-
-factor=0.75
-angle=30
-sigma=5
-tom=Turtle()
-
-tom.penup()
-tom.goto(x=0,y=-150)
-tom.left(90)
-tom.pendown()
-
-dendrite(200,7)
+if __name__ == "__main__":
 
 
 
-tom.hideturtle()
+    jacob=Turtle()
+
+    jacob.penup()
+    jacob.goto(x=0,y=-150)
+    jacob.left(90)
+    jacob.pendown()
+    
+    jacob.speed(0)
+    dendrite(150,7,5,.75,90,jacob)
+
+
+
+    jacob.hideturtle()
 
 #these two lines are for saving the image
-ts = tom.getscreen()
-ts.getcanvas().postscript(file="simple_dendrite.eps")
+    ts = jacob.getscreen()
+    ts.getcanvas().postscript(file="simple_dendrite.eps")
 
-tom.getscreen()._root.mainloop()
+    jacob.getscreen()._root.mainloop()
 
+ 
---
+++ python_workshop/draw_a_neuron/simple_neuron.py
@@ -0,0 +1,61 @@
+from turtle import *
+from math import *
+import random as rnd
+from simple_dendrite_more_branches import *
+
+jacob=Turtle()
+jacob.up()
+jacob.goto(x=0,y=-100)
+jacob.down()
+jacob.speed(0)
+jacob.width(3)
+
+turns=[]
+
+
+for i in range(45):
+    jacob.forward(0.75)
+    turns.append(rnd.normalvariate(2,1.5))
+    jacob.right(turns[-1])
+for i in range(10):
+    jacob.forward(2)
+    turns.append(rnd.normalvariate(0,2))
+    jacob.right(turns[-1])
+for i in range(45):
+    jacob.forward(0.75)
+    turns.append(rnd.normalvariate(2,1.5))
+    jacob.right(turns[-1])
+
+x=jacob.xcor()
+y=jacob.ycor()
+jacob.width(2)
+jacob.setheading(rnd.normalvariate(270,10))
+for i in range(50):
+    jacob.forward(2)
+    jacob.left(rnd.normalvariate(0,2))
+jacob.up()
+jacob.setx(x)
+jacob.sety(y)
+jacob.down()
+jacob.setheading(180)
+jacob.width(3)
+turns.reverse()
+for i in range(45):
+    jacob.forward(0.75)
+    jacob.right(turns.pop())
+for i in range(10):
+    jacob.forward(2)
+    jacob.right(turns.pop())
+for i in range(45):
+    jacob.forward(0.75)
+    jacob.right(turns.pop())
+
+jacob.setheading(90)
+branch_n=5
+factor=0.8
+dendrite(150,7,5,0.8,60,jacob)
+
+#jacob.goto(x=0,y=-150)
+
+
+jacob.getscreen()._root.mainloop()
